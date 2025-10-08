e22643bff1fb50a9430c40fd122dc957491f0e3a
kzaidova <katerina00.00@bk.ru>
2025-10-08 18:16:45 +0700

    Обновила данные

---
+++ FirstLesson/FirstLesson.py
@@ -0,0 +1,6 @@
+#fisrt_lesson
+user_name = input("Привет, познакомимся? Напиши свое имя:")
+print(f"Приятно познакомиться, {user_name}!")
+age = input("напиши сколько тебе лет, в цифрах:")
+one_hundred_years = (2025 + (100 - int(age)))
+print(f"Ого, тебе исполнится сто лет в {one_hundred_years} году!")
